837f4c370a2332d9e75d09d942c2494f89928186
horsellcommon <crawfordj716@gmail.com>
2022-10-03 15:29:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([])
+
+  const randNumber = Math.floor(Math.random() * 9)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://swapi.dev/api/people/");
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch("");
+  //     const data1 = await response.json();
+  //     setData1(data1);
+  //     console.log(data1);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch("");
+  //     const data2 = await response.json();
+  //     setData2(data2);
+  //     console.log(data2);
+  //   };
+  //   fetchData();
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>What Star Wars Character Are You?</h1>
+      <h2>TEMP</h2>
+      <button>TEMP</button>
     </div>
   );
 }
